b733bd6802af800d4f312e5e08a5f33b98423b10
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-07-09 20:47:52 +0200

    added parts of Error.js and Forbidden.js

---
+++ client/src/components/Error.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => (
+
+    <div className="bounds">
+        <h1>Forbidden</h1>
+        <p>Oh oh! You can't access this page.</p>
+    </div>
+);
+
+
---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => (
+
+    <div className="bounds">
+        <h1>Error</h1>
+        <p>Sorry! We just encountered an unexpected error.</p>
+    </div>
+);
